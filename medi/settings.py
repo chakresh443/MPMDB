9a991dfc53b7982ab054efeccf2dde622d0b1e44
gyashika-01 <gyashika-01@gmail.com>
2025-04-23 14:17:37 +0530

    Initial commit

--- medi/settings.py
+++ medi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wc!bkp6gr0p)rc&s1)jkmu+!^1v$=z%lha20h692%25l&tg+j0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'mpmdb.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mpmdb.onrender.com', 'mpmetdb.onrender.com']
 
 
 # Application definition
